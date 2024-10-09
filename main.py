fe66cd581489c7d9d0c0a6dc26e184b5be89cd6f
dominjk <dominik@salena.com.pl>
2024-10-09 13:22:06 +0200

    add database

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify, abort
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 import lotterySIM
 from lotteries import LOTTERIES
+from copy import deepcopy
 
 
 PORT = 5000
@@ -10,23 +11,20 @@ HOST = "127.0.0.1"
 
 app = Flask(__name__)
 api = Api(app)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-# db = SQLAlchemy(app)
 
 
-# class LotteryModel(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+class SimulationModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    simulation = db.Column(db.PickleType, nullable=False)
     
+    def __repr__(self):
+        return f"Simulation({self.id}, {self.simulation.get_results()})"
 
-# db.create_all()
-
-
-# kolizje id
-# nadaj id w zaleleznosci do klienta
-# usun z listy gdy polaczenie zerwane
-
-
-simulation_sessions = {}
+db.drop_all()
+db.create_all()
 
 
 simulation_create_args = reqparse.RequestParser()
@@ -37,24 +35,27 @@ simulation_get_args = reqparse.RequestParser()
 simulation_get_args.add_argument("speed", type=int, help="Number of year's passed every request", required=False)
 
 
+
 class Lottery(Resource):
-    def get(self, id):
+    def patch(self, id):
         args = simulation_get_args.parse_args()
-        sim_data = simulation_sessions.get(id)
-        
-        if sim_data is None:
-            abort(404, "Simulation ID is not valid")
+        simulation_model = SimulationModel.query.get_or_404(id)
             
         speed = args.get("speed")
         # 200 years per request limit
         speed = min(speed, 200) if speed is not None else 1 
 
-        simulation = sim_data["simulation"]
+        simulation = deepcopy(simulation_model.simulation)
         
         simulation.simulate_years(speed)
-        data = simulation.get_results()
+        result_data = simulation.get_results()
         
-        return jsonify(data)
+        simulation_model.simulation = simulation 
+        
+        db.session.add(simulation_model)
+        db.session.commit()
+        
+        return jsonify(result_data)
 
     
     def put(self, id):
@@ -80,17 +81,22 @@ class Lottery(Resource):
             lottery, player, rounds_per_week=3
         )
         
-        simulation_sessions[id] = {"simulation": simulation}
+        if SimulationModel.query.get(id) is None:
+            simulation_model = SimulationModel(id=id, simulation=simulation)
+            db.session.add(simulation_model)
+            db.session.commit()
+        else:
+            abort(400, "ID already exists") 
+        
         return {"lottery": lottery_name, "simulation_id": id, "guess": player.get_guess()}, 201
     
+    
     def delete(self, id):
-        if id in simulation_sessions:
-            del simulation_sessions[id]
-        else:
-            abort(400, "Simulation ID is not valid")
+        SimulationModel.query.filter_by(id=id).delete()
+        db.session.commit()
+        
     
-    
-api.add_resource(Lottery, "/lottery/<string:id>")
+api.add_resource(Lottery, "/lottery/<int:id>")
 
 
 if __name__ == "__main__":
